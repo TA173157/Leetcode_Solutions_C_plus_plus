40bdf7c201a3c5a86d879891c8a2dfcbc83f68cf
TA173157 <tamusic2147k@gmail.com>
2025-10-25 16:54:08 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.55%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
-        while(n!=1){
-            if(n%2==1)return false;
-            n/=2;
-        }
-        return true;
+        return ((n&(n-1))==0);
+    
     }
 };
