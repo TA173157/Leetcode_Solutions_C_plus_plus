3ff76bc88ea97dd58053eb3861e4f7536e7da6a3
TA173157 <tamusic2147k@gmail.com>
2025-11-02 19:48:24 +0530

    Time: 32 ms (24.82%), Space: 18.1 MB (25.08%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -4,20 +4,19 @@ public:
         if(s.size()==1)return s;
         set<char> target_chars = {'a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U'};
         vector<char>lower_char;
-        vector<int>lower;
+        
         int i=0;
    for (const char& c : s) {
         if (target_chars.count(c)) {
-            lower_char.push_back(c) ;lower.push_back(i); 
+            lower_char.push_back(c) ;s[i]='#'; 
         }
         i++;
     } 
-    i=0;
+    i=0;int k=0;
     sort(lower_char.begin(),lower_char.end());
    
-       while( lower_char.size()>i){
-       
-        s[lower[i]]= lower_char[i];
+       while( s.size()>i){
+       if(s[i]=='#'){s[i]= lower_char[k];k++;}
         i++;
        }
        return s;
