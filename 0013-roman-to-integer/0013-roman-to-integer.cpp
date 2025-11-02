1d597b3ed6864bc35394b068073eb9a5c94f4b4c
TA173157 <tamusic2147k@gmail.com>
2025-11-03 01:36:53 +0530

    Time: 0 ms (100%), Space: 9.9 MB (99.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+  int getValue(char r) {
+    switch (r) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0; // Should not happen with valid input
+    }
+}
     int romanToInt(string s) {
-        map<char,int> roman; int num=0,prev=1000,sum=0;
-        roman['I']=1;
-        roman['V']=5;
-        roman['X']=10;
-        roman['L']=50;
-        roman['C']=100;
-        roman['D']=500;
-        roman['M']=1000;
+     int num=0,prev=1000,sum=0;
         for(int i=0;i<s.size();i++){
-           num= roman[s[i]];
-            if(num>prev) {sum-=prev;
-            sum+=num-prev;}
+           num= getValue(s[i]);
+            if(num>prev) {
+            sum+=num-2*prev;}
            else{sum+= num;}
            prev = num;
         }
