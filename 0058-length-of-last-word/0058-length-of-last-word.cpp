7aed43bacf52dcb766ae44e402e8bbc197a5329f
TA173157 <tamusic2147k@gmail.com>
2025-11-15 21:21:55 +0530

    Time: 3 ms (2.69%), Space: 9.5 MB (7.18%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         string a="",b="";
-        for(int i=0;i<s.size();i++){
+        for(int i=s.size()-1;i>=0;i--){
             if(s[i]==' '){
+                if(b.size()!=0) return b.size();
                 a="";continue;
             }
-            else {a+=s[i];b=a; }
+            else {a=s[i]+a;b=a; }
         }
         return b.size();
     }
