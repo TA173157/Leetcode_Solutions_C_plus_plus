fa0c8ad9addf19b5b32d729030f2147554c777e8
TA173157 <tamusic2147k@gmail.com>
2025-11-16 22:30:30 +0530

    Time: 83 ms (10.92%), Space: 65 MB (16.19%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        set<int>num;
-        for(int a : nums){
-            num.insert(a);
-        }
+        set<int> num(nums.begin(), nums.end());
+       
         int size = nums.size();
-        // vector<int>result;
         nums.clear();
         for(int i=0;i<size;i++){
-            
             if(!num.count(i+1)) nums.push_back(i+1);
         }
         return nums;
