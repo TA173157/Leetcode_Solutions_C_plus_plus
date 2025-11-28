9de2b859a8fb75b5e299c04e534936578ab76baa
TA173157 <tamusic2147k@gmail.com>
2025-11-28 23:00:35 +0530

    Time: 0 ms (100%), Space: 31.6 MB (17.89%) - LeetHub

--- 3736-minimum-moves-to-equal-array-elements-iii/3736-minimum-moves-to-equal-array-elements-iii.cpp
+++ 3736-minimum-moves-to-equal-array-elements-iii/3736-minimum-moves-to-equal-array-elements-iii.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int maxi = nums[nums.size()-1];
+        int maxi=1;
+        for(int i=0;i<nums.size();i++){
+            if(maxi<nums[i])maxi=nums[i];
+        }
         int sum=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==maxi)continue;
